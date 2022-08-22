jarrpa/ocs-consumer-operator

controllers: add StorageClassClaim api and controller
Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "StorageConsumerClient")
 		os.Exit(1)
 	}
+	if err = (&controllers.StorageClassClaimReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StorageConsumerClient")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {

red-hat-storage/ocs-client-operator

main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OcsClient")
 		os.Exit(1)
 	}
+	if err = (&controllers.StorageClassClaimReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StorageClassClaim")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {