4d592232f010cdf57156208202a3176a99ac06d4
Madhu Rajanna <madhupr007@gmail.com>
2023-01-24 11:36:39 +0100

    controllers: add storageclient controller
    Adding storageClient CR which supports
    anboarding and offboarding new storageclients
    to the provider cluster.
    
    A single storageclient can exists in a single
    namespace. If someone tries to create one
    more storageclient it will fail.
    
    Co-authored-by: Jose A. Rivera <jarrpa@redhat.com>
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- main.go
+++ main.go
@@ -106,6 +106,13 @@ func main() {
 	}
 
 	//+kubebuilder:scaffold:builder
+	if err = (&controllers.StorageClientReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "StorageClient")
+		os.Exit(1)
+	}
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
