ddf9be606f8bad53ed156f892904ba5ff03cbc4d
Madhu Rajanna <madhupr007@gmail.com>
2023-02-07 11:40:27 +0100

    controllers: create status-reporter job in operator ns
    currently status-reporter job is created in the namespace
    where storageclient gets created. To have a running pod
    we need to have RBAC and servicAccount created on the
    same namespace. We dont want to create the RBAC
    from the code as it will get skipped in the security
    audit and its a security problem. To solve it
    when a storageclient gets created the status
    reporter job will get created in the operator
    namespace and it will get deleted when the
    storageclient gets deleted.
    
    Signed-off-by: Madhu Rajanna <madhupr007@gmail.com>

--- main.go
+++ main.go
@@ -112,8 +112,9 @@ func main() {
 	}
 
 	if err = (&controllers.StorageClientReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
+		Client:            mgr.GetClient(),
+		Scheme:            mgr.GetScheme(),
+		OperatorNamespace: utils.GetOperatorNamespace(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "StorageClient")
 		os.Exit(1)
