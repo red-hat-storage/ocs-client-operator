19e5a1cb5fdcc024d4c687c3e8f632b373889d48
Jose A. Rivera <jarrpa@redhat.com>
2023-03-27 11:27:37 -0500

    hack: add CSI_IMAGES_MANIFEST variable
    Signed-off-by: Jose A. Rivera <jarrpa@redhat.com>

--- hack/gen-csi-images-manifest.sh
+++ hack/gen-csi-images-manifest.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 
-cd config/manager
-rm -f csi-images.yaml
+CSI_IMAGES_MANIFEST="${CSI_IMAGES_MANIFEST:-config/manager/csi-images.yaml}"
 
 echo "Generating CSI image manifest for OCP versions: ${CSI_OCP_VERSIONS}"
 
-echo -n "---" > csi-images.yaml
+rm -f "${CSI_IMAGES_MANIFEST}"
+echo -n "---" > "${CSI_IMAGES_MANIFEST}"
 for version in ${CSI_OCP_VERSIONS}; do
-  echo "" >> csi-images.yaml
+  echo "" >> "${CSI_IMAGES_MANIFEST}"
 
   ver="${version//./_}"
-  echo -e "- version: $version\n  containerImages:" >> csi-images.yaml
+  echo -e "- version: $version\n  containerImages:" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_PROVISIONER_${ver}"
-  echo "    provisionerImageURL: \"${!csi_var:-${CSI_IMG_PROVISIONER}}\"" >> csi-images.yaml
+  echo "    provisionerImageURL: \"${!csi_var:-${CSI_IMG_PROVISIONER}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_ATTACHER_${ver}"
-  echo "    attacherImageURL: \"${!csi_var:-${CSI_IMG_ATTACHER}}\"" >> csi-images.yaml
+  echo "    attacherImageURL: \"${!csi_var:-${CSI_IMG_ATTACHER}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_RESIZER_${ver}"
-  echo "    resizerImageURL: \"${!csi_var:-${CSI_IMG_RESIZER}}\"" >> csi-images.yaml
+  echo "    resizerImageURL: \"${!csi_var:-${CSI_IMG_RESIZER}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_SNAPSHOTTER_${ver}"
-  echo "    snapshotterImageURL: \"${!csi_var:-${CSI_IMG_SNAPSHOTTER}}\"" >> csi-images.yaml
+  echo "    snapshotterImageURL: \"${!csi_var:-${CSI_IMG_SNAPSHOTTER}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_REGISTRAR_${ver}"
-  echo "    driverRegistrarImageURL: \"${!csi_var:-${CSI_IMG_REGISTRAR}}\"" >> csi-images.yaml
+  echo "    driverRegistrarImageURL: \"${!csi_var:-${CSI_IMG_REGISTRAR}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_CEPH_CSI_${ver}"
-  echo "    cephCSIImageURL: \"${!csi_var:-${CSI_IMG_CEPH_CSI}}\"" >> csi-images.yaml
+  echo "    cephCSIImageURL: \"${!csi_var:-${CSI_IMG_CEPH_CSI}}\"" >> "${CSI_IMAGES_MANIFEST}"
 
   csi_var="CSI_IMG_ADDONS_${ver}"
-  echo "    csiaddonsImageURL: \"${!csi_var:-${CSI_IMG_ADDONS}}\"" >> csi-images.yaml
+  echo "    csiaddonsImageURL: \"${!csi_var:-${CSI_IMG_ADDONS}}\"" >> "${CSI_IMAGES_MANIFEST}"
 done
