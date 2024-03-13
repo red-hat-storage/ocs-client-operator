9cebafc2d827cadda0a3f5526fd5e0aa767bba3a
Rewant Soni <resoni@redhat.com>
2024-03-13 10:43:46 +0530

    makefile: allow images to be build using both podman and docker
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- hack/make-project-vars.mk
+++ hack/make-project-vars.mk
@@ -10,3 +10,11 @@ GOARCH ?= amd64
 GO_LINT_IMG_LOCATION ?= golangci/golangci-lint
 GO_LINT_IMG_TAG ?= v1.56.2
 GO_LINT_IMG ?= $(GO_LINT_IMG_LOCATION):$(GO_LINT_IMG_TAG)
+
+ifeq ($(IMAGE_BUILD_CMD),)
+IMAGE_BUILD_CMD := $(shell command -v docker || echo "")
+endif
+
+ifeq ($(IMAGE_BUILD_CMD),)
+IMAGE_BUILD_CMD := $(shell command -v podman || echo "")
+endif
