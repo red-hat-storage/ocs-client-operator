7a054ef6e8e414e745d7fc2661017a3e1cb48a5e
Rewant Soni <resoni@redhat.com>
2024-03-13 11:23:21 +0530

    makefile: update opm version to 1.20.0
    update opm version to be run on all os/arch
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- hack/make-tools.mk
+++ hack/make-tools.mk
@@ -31,4 +31,4 @@ operator-sdk: ## Download operator-sdk locally if necessary.
 .PHONY: opm
 OPM = $(BIN_DIR)/opm
 opm: ## Download opm locally if necessary.
-	@./hack/get-tool.sh $(OPM) https://github.com/operator-framework/operator-registry/releases/download/v1.15.1/$(GOOS)-$(GOARCH)-opm
+	@./hack/get-tool.sh $(OPM) https://github.com/operator-framework/operator-registry/releases/download/v1.20.0/$(GOOS)-$(GOARCH)-opm
