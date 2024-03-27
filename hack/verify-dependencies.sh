22178de98554322786ec31b3e8f29a56e678c38b
Leela Venkaiah G <lgangava@ibm.com>
2024-03-27 15:35:23 +0530

    ci: verify go dependencies update
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

---
+++ hack/verify-dependencies.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+paths=('go.mod' 'go.sum' 'vendor/')
+
+if [[ -n "$(git status --porcelain "${paths[@]}")" ]]; then
+	git diff -u "${paths[@]}"
+	echo "uncommitted dependency files. run 'make godeps-update' and commit results."
+	exit 1
+fi
+echo "Success: no out of source tree changes found for dependency files"
