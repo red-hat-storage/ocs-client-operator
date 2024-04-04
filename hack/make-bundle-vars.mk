b27e92b8016e382f36c30aa65ee9a9c3cf21b697
Leela Venkaiah G <lgangava@ibm.com>
2024-04-04 17:02:17 +0530

    bundle: update csi-addons dependency
    since we are going to be installed alongside w/ odf-operator inorder to
    conform to u/s CI changing the dependency of csi-addons from 0.5.0 to
    0.8.0
    
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- hack/make-bundle-vars.mk
+++ hack/make-bundle-vars.mk
@@ -49,7 +49,7 @@ IMAGE_TAG ?= latest
 IMAGE_NAME ?= ocs-client-operator
 BUNDLE_IMAGE_NAME ?= $(IMAGE_NAME)-bundle
 CSI_ADDONS_BUNDLE_IMAGE_NAME ?= k8s-bundle
-CSI_ADDONS_BUNDLE_IMAGE_TAG ?= v0.5.0
+CSI_ADDONS_BUNDLE_IMAGE_TAG ?= v0.8.0
 CATALOG_IMAGE_NAME ?= $(IMAGE_NAME)-catalog
 
 OCS_CLIENT_CONSOLE_IMG_NAME ?= ocs-client-console
@@ -99,7 +99,7 @@ endif
 
 # csi-addons dependencies
 CSI_ADDONS_PACKAGE_NAME ?= csi-addons
-CSI_ADDONS_PACKAGE_VERSION ?= 0.5.0
+CSI_ADDONS_PACKAGE_VERSION ?= 0.8.0
 
 ## CSI driver images
 # The following variables define the default CSI container images to deploy
