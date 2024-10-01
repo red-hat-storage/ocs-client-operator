ac7a42b288122eda86abf511f1eb95b685618f93
Leela Venkaiah G <lgangava@ibm.com>
2024-10-01 11:00:33 +0530

    api: move into a separate go module
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- hack/godeps-verify.sh
+++ hack/godeps-verify.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-paths=('go.mod' 'go.sum' 'vendor/')
+paths=('go.mod' 'go.sum' 'vendor/' 'api/go.mod' 'api/go.sum' 'api/vendor/')
 
 if [[ -n "$(git status --porcelain "${paths[@]}")" ]]; then
 	git diff -u "${paths[@]}"
-	echo "Inconsistency found in dependency files. Run 'make update-go-deps' and commit results."
+	echo "Inconsistency found in dependency files. Run 'make godeps-update' and commit results."
 	exit 1
 fi
 echo "Success: no out of source tree changes found for dependency files"
