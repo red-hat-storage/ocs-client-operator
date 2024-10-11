4e1649516fa8599297771078869c5157f3a81922
Rewant Soni <resoni@redhat.com>
2024-10-11 16:09:11 +0530

    hack: fix the entrypoint script
    Signed-off-by: Rewant Soni <resoni@redhat.com>

--- hack/entrypoint.sh
+++ hack/entrypoint.sh
@@ -3,7 +3,7 @@
 RESTART_EXIT_CODE=42
 
 while true; do
-    ./usr/local/bin/ocs-operator $@
+    ./manager $@
     EXIT_CODE=$?
     if [ $EXIT_CODE -ne $RESTART_EXIT_CODE ]; then
       exit $EXIT_CODE
