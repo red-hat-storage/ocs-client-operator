cbce85df782b19fa6fcf4110bc5293f1f44e7492
Leela Venkaiah G <lgangava@ibm.com>
2024-12-18 13:13:51 +0000

    controllers: testing the waters for convergence
    Signed-off-by: Leela Venkaiah G <lgangava@ibm.com>

--- cmd/main.go
+++ cmd/main.go
@@ -181,15 +181,6 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = (&controller.StorageClaimReconciler{
-		Client:            mgr.GetClient(),
-		Scheme:            mgr.GetScheme(),
-		OperatorNamespace: utils.GetOperatorNamespace(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "StorageClaim")
-		os.Exit(1)
-	}
-
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
