1801abf618b0c229fc0eb8d751c07ede7e98edf9
ShravaniVangur <shravanivangur@gmail.com>
2025-04-24 10:57:13 +0530

    controllers: restrict manager to watch selective  namespaces
    Cache resources from namespaces where the operator is deployed or that
    are specified in  the `WATCH_NAMESPACE` env variable.
    (cherry picked from commit 4531319c3f3a508008632d235069d979d38bbb81)
    
    Signed-off-by: ShravaniVangur <shravanivangur@gmail.com>

--- pkg/utils/k8sutils.go
+++ pkg/utils/k8sutils.go
@@ -30,6 +30,10 @@ import (
 // which is the namespace where operator pod is deployed.
 const OperatorNamespaceEnvVar = "OPERATOR_NAMESPACE"
 
+// WatchNamespaceEnvVar is the constant for env variable WATCH_NAMESPACE
+// which indicates any other namespace to watch for resources.
+const WatchNamespaceEnvVar = "WATCH_NAMESPACE"
+
 // OperatorPodNameEnvVar is the constant for env variable OPERATOR_POD_NAME
 const OperatorPodNameEnvVar = "OPERATOR_POD_NAME"
 
@@ -53,6 +57,10 @@ const CSIReconcileEnvVar = "CSI_RECONCILE"
 // GetOperatorNamespace returns the namespace where the operator is deployed.
 func GetOperatorNamespace() string {
 	return os.Getenv(OperatorNamespaceEnvVar)
+}
+
+func GetWatchNamespace() string {
+	return os.Getenv(WatchNamespaceEnvVar)
 }
 
 func ValidateOperatorNamespace() error {
